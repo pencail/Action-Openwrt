bcb10d81c82dd378c97addd222270878a852d0e4
pencail <473632514@qq.com>
2024-07-13 00:00:33 +0800

    Update diy-part1.sh

--- build/openwrt_23.05.0/diy-part1.sh
+++ build/openwrt_23.05.0/diy-part1.sh
@@ -17,17 +17,10 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
-#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
-#echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
-
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git pencail https://github.com/pencail/openwrt-packages' >>feeds.conf.default
 
-
+#配置argon主题
 git clone --branch luci-21 https://github.com/jjm2473/luci-theme-argon.git package/luci-theme-argon
 git clone --branch dev https://github.com/jjm2473/luci-app-argon-config.git package/luci-app-argon-config
  
