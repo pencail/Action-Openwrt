f3ccb7a8d797b82bd43872fe1809de13093175b7
pencail <473632514@qq.com>
2025-07-16 21:08:41 +0800

    更新23.05.5版本

--- build/openwrt_23.05.5/diy-part1.sh
+++ build/openwrt_23.05.5/diy-part1.sh
@@ -21,7 +21,7 @@
 echo 'src-git pencail https://github.com/pencail/openwrt-packages' >>feeds.conf.default
 
 #配置argon主题
-git clone --branch luci-21 https://github.com/jjm2473/luci-theme-argon.git package/luci-theme-argon
+git clone --branch luci-24 https://github.com/jjm2473/luci-theme-argon.git package/luci-theme-argon
 git clone --branch dev https://github.com/jjm2473/luci-app-argon-config.git package/luci-app-argon-config
 #修改登录按钮能够正常汉化
 sed -i '152s/Login/Log in/g' package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
--- build/openwrt_23.05.5/diy-part2.sh
+++ build/openwrt_23.05.5/diy-part2.sh
@@ -22,3 +22,15 @@
 #sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 #sed -i 's/%D %V %C/OpenWrt By Pencail/g' package/base-files/files/etc/openwrt_release
 #sed -i 's/%C/By Pencail/g' package/base-files/files/etc/banner
+
+# sed语法，\用来标记特殊符号，如果字符中有特殊符号，前边加上\标记
+#删除源码中自动生成内核md5的代码
+#sed -i '130d' include/kernel-defaults.mk
+#复制官方内核md5到编译中
+#sed -i '129a cp $(TOPDIR)/.vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
+#sed -i '130s/.*/cp \$(TOPDIR)\/\.vermagic \$(LINUX_DIR)\/\.vermagic/g' include/kernel-defaults.mk
+sed -i '130s#.*#\tcp $(TOPDIR)/.vermagic $(LINUX_DIR)/.vermagic#g' include/kernel-defaults.mk
+
+#删除软件仓库中luci-app-adguardhome或者adguardhome的初始化脚本
+#rm -f feeds/pencail/luci-app-adguardhome/root/etc/init.d/adguardhome
+sed -i '/INSTALL_BIN/d' feeds/packages/net/adguardhome/Makefile
