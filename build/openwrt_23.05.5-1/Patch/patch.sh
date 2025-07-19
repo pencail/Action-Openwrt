94acb60f2f0f571093eb1289216b2211c3556d7b
pencail <473632514@qq.com>
2025-07-19 21:18:05 +0800

    修改补丁命令

--- build/openwrt_23.05.5-1/Patch/patch.sh
+++ build/openwrt_23.05.5-1/Patch/patch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 将补丁文件复制到openwrt根目录
-cp -Rf $GITHUB_WORKSPACE/build/${{github.event.inputs.target}}/Patch/patches patches
-
 # 启用补丁文件
 cat >> .config << 'EOF'
 SOURCE_TREE_OVERRIDE=YES
